af6557d01432d5b99748d0fdb7ea3afdd564a502
Filipe Avanzzo <avanzzodev@gmail.com>
2024-03-30 23:09:20 -0300

    fix: fix hover effect

--- src/pages/blog/components/Card.jsx
+++ src/pages/blog/components/Card.jsx
@@ -12,7 +12,7 @@ export function Card({ title, subtitle, id, isLast }) {
       className={` group flex h-64 w-full cursor-pointer  flex-col justify-center gap-4  rounded-md border bg-sky-500 p-8 text-neutral-300 shadow-md transition-all  duration-300 hover:bg-sky-700 hover:opacity-90  ${isLast ? " mb-12 sm:col-span-2 sm:items-center md:col-auto md:items-start" : "items-start"}`}
     >
       <Logo className="w-20 self-center " />
-      <h2 className="group-hover text-2xl font-bold text-sky-500 group-hover:text-white group-hover:opacity-80">
+      <h2 className="group-hover text-2xl font-bold group-hover:text-white group-hover:opacity-80">
         {title}
       </h2>
       <p className="">{subtitle}</p>
